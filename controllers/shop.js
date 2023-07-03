44e6fd52d1b008461a4f92ec9df88ec6c4b9b12e
Riya Dutta <riya2001dutta@gmail.com>
2023-07-04 05:25:49 +0530

    wrapping up with sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 //with this syntax you can add mutiple exports in one file..
 
@@ -114,11 +113,45 @@ exports.postCartDeleteProduct = (req, res, next)=>{
   .catch(err => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+  .getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts();
+  })
+  .then(products => {
+    return req.user
+    .createOrder()
+    .then(order => {
+      return order.addProducts(products.map(product => {
+        product.orderItem = {quantity : product.cartItem.quantity};
+        return product;
+      }));
+    }) 
+  .catch(err => console.log(err));
+  })
+  .then(result => {
+    return fetchedCart.setProducts(null); //to clean up cart after moving everything to orders table..
+  })
+  .then(result => {
+    res.redirect('/orders');
+  })
+  .catch(err => console.log(err));
+}
+
 exports.getOrders = (req,res,next)=>{ 
-  res.render('shop/orders', {
-    pageTitle: 'Your Orders',
-    path: '/orders'
-  });
+  req.user.getOrders({include: ['products']})
+  .then(orders => {
+    res.render('shop/orders', {
+      pageTitle: 'Your Orders',
+      path: '/orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
+  
 }
 
 exports.getCheckout = (req,res,next)=>{
